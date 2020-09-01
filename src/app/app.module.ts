91388587593db7dc0f7c2437aac2c1a0e4adfcfd
Daniel P. WOAGOU <daniwx@gmail.com>
2020-09-01 19:27:43 +0000

    Mise en forme ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AcceuilComponent } from './acceuil/acceuil.component';
+import { ListProjetComponent } from './list-projet/list-projet.component';
+import { DetailProjetComponent } from './detail-projet/detail-projet.component';
+import { FormProjetComponent } from './form-projet/form-projet.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AcceuilComponent
+    AcceuilComponent,
+    ListProjetComponent,
+    DetailProjetComponent,
+    FormProjetComponent,
+    AproposComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,

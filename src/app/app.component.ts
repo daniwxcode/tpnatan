7649805abbee2ef15190ed885eb225e7c18e2a12
WOAGOU Daniel <daniwx@gmail.com>
2020-09-18 10:15:17 +0000

    images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,25 +7,82 @@ import { ConnexionComponent } from './user/connexion/connexion.component'
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
-  private menuService : MenuService;
-  //afficherMenu : boolean = menuService
-  afficherAccueil: boolean= true;
+export class AppComponent implements OnInit {
+  private menuService: MenuService;
+  afficherAccueil: boolean = true;
   afficherMenu: boolean = true;
 
-  title = 'Natan';
-  hideAccueil(){
+  title = 'Nataan';
+  hideAccueil() {
     this.afficherAccueil = false;
     this.title = 'Bonjour';
   }
-  hideMenu(){
+  hideMenu() {
     this.afficherMenu = false;
   }
 
-  ngOnInit(){
+  ngOnInit() {
     this.afficherAccueil = true;
+    this.acceuil = "active";
     this.afficherMenu = true;
   }
- 
+  public acceuil: string;
+  public about: string;
+  public list: string;
+  public details: string;
+  public contact: string;
+
+  active(menu: string) {
+    switch (menu) {
+      case "acceuil": {
+        this.acceuil = "active";
+        this.about = "";
+        this.list = "";
+
+        break;
+      }
+
+      case "about": {
+        this.acceuil = "";
+        this.about = "active";
+        this.list = "";
+        this.details = "";
+        this.contact = "";
+
+        break;
+      }
+      case "list":
+        {
+          this.acceuil = "";
+          this.about = "";
+          this.list = "active";
+          this.details = "";
+          this.contact = "";
+
+          break;
+        }
+      case "details":
+        {
+          this.acceuil = "";
+          this.about = "";
+          this.list = "";
+          this.details = "active";
+          this.contact = "";
+
+          break;
+        }
+      case "contact":
+        {
+          this.acceuil = "";
+          this.about = "";
+          this.list = "";
+          this.details = "";
+          this.contact = "active";
+
+          break;
+        }
+    }
+  }
+
 
 }
